7a437303d09fee91fb6b0b2047f504d56765dfeb
Karen Da Cruz <me@karendacruz.com>
2023-10-11 23:48:58 -0500

    Test for burning token

--- test/test.js
+++ test/test.js
@@ -11,22 +11,58 @@ const BURNER_ROLE = getRole("BURNER_ROLE");
 // 00 horas del 30 de septiembre del 2023 GMT
 var startDate = 1696032000;
 
-var owner, alice;
+var owner, alice, signerWhitelist;
+
+const DECIMALS_BBTKN = 6;
+
+const proofs = [
+    '0x9682923cee08a0d2c343f252beae1ef85540e977e80fa686488f9f10ea4685ec',
+    '0x48781e8f88ab4d1d07af285d1367732f0dcc4e6f5704640f66ea3c8d3864b713',
+    '0xa05e993355027b3037785d2f63c3363ab61b69e430d4054e364c9566dfb8d0ee',
+    '0xa8e78c35b8fdaa5a0f307cd1d8b12dfa9d0fd99b528ade7bfcbf2b467e35cb6c',
+    '0x7e6e241807eba959c7e886d66fbb24ed7eb9704acfa05c05c31ab7dd5c7d8325',
+    '0x839e6099895ad99f1b5195b401d7b131744def11071dfe8e463f6a0ac1d918a0',
+    '0x7da7c7f5098b4704e5564d19f6d65bc3f551112e46081cd0b70c8cfab4d0a9b0',
+    '0x9a184d9a1744e8e9093ac5e65d7e96c1b609549a75d38c610f538aead2b04c58',
+    '0x7bb0854a5a2a180c8416993d26910d3100cd9f9bc389fb8ac6449d3e37c467d7',
+    '0x7f818777dfe171b443c8a7d57bd2d50e26850f9bd2640b905e5e0b82d205818c'
+  ];
 
 before(async () => {
-    [owner, alice, bob] = await ethers.getSigners();
+    [owner, alice] = await ethers.getSigners();
+    whitelistSigner = await hre.ethers.getSigner("0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4");
+    // var whitelistAddress = "0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4";
+    // signerWhitelist = await ethers.getSigner(whitelistAddress);
+    // await network.provider.send("hardhat_setBalance", [
+    //     addressA,
+    //     "0x1000", //valor en hexadecimal
+    //   ]);
 });
 
 describe("Set up", function () {
     it("Publicar los contratos", async () => {
         // Publicar BBTKN 
         BBitesToken = await hre.ethers.getContractFactory("BBitesToken");
-        contract_BBitesToken = await BBitesToken.deploy();
+        // contract_BBitesToken = await BBitesToken.deploy();
+        contract_BBitesToken = await hre.upgrades.deployProxy(BBitesToken, {
+            kind: "uups",
+        });
+        var implementationAddress = await hre.upgrades.erc1967.getImplementationAddress(
+            contract_BBitesToken.target
+        );
+        // console.log(`El address del Proxy es ${contract_BBitesToken.target}`);
+        // console.log(`El address de la implementacion es ${implementationAddress}`);
 
-        // Set up Roles
-        CuyCollectionNFT = await hre.ethers.getContractFactory("CuyCollectionNft");
-        contract_CuyCollectionNFT = await CuyCollectionNFT.deploy("Cuy Collection NFT", "CUYNFT");
+        // // Publicar CuyNFT
+        CuyCollectionNFT = await hre.ethers.getContractFactory("CuyCollectionNFT");
+        // contract_CuyCollectionNFT = await CuyCollectionNFT.deploy("Cuy Collection NFT", "CUYNFT");
+        contract_CuyCollectionNFT = await hre.upgrades.deployProxy(CuyCollectionNFT, {
+            kind: "uups",
+        });
         
+        // // Publicar Public Sale
+        // PublicSale = await hre.ethers.getContractFactory("PublicSale");
+        // contract_PublicSale = await PublicSale.deploy();
     });
 });
 
@@ -65,18 +101,6 @@ describe("Cuy Collection NFT - MINT", function () {
         expect(tokenOwner).to.equal(alice.address, "Minter should have minted the token for the wallet");
     });
     it("validates wallet within Merkle Tree", async() => {
-        var proofs = [
-            '0x9682923cee08a0d2c343f252beae1ef85540e977e80fa686488f9f10ea4685ec',
-            '0x48781e8f88ab4d1d07af285d1367732f0dcc4e6f5704640f66ea3c8d3864b713',
-            '0xa05e993355027b3037785d2f63c3363ab61b69e430d4054e364c9566dfb8d0ee',
-            '0xa8e78c35b8fdaa5a0f307cd1d8b12dfa9d0fd99b528ade7bfcbf2b467e35cb6c',
-            '0x7e6e241807eba959c7e886d66fbb24ed7eb9704acfa05c05c31ab7dd5c7d8325',
-            '0x839e6099895ad99f1b5195b401d7b131744def11071dfe8e463f6a0ac1d918a0',
-            '0x7da7c7f5098b4704e5564d19f6d65bc3f551112e46081cd0b70c8cfab4d0a9b0',
-            '0x9a184d9a1744e8e9093ac5e65d7e96c1b609549a75d38c610f538aead2b04c58',
-            '0x7bb0854a5a2a180c8416993d26910d3100cd9f9bc389fb8ac6449d3e37c467d7',
-            '0x7f818777dfe171b443c8a7d57bd2d50e26850f9bd2640b905e5e0b82d205818c'
-          ];
         var leaf = Buffer.from(
             ethers
               .solidityPackedKeccak256(["uint256", "address"], [1002, "0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4"])
@@ -91,18 +115,6 @@ describe("Cuy Collection NFT - MINT", function () {
         await expect(result).to.equal(true, "Doesn't recognize the wallet ID to belong to the Merkle tree");
     });
     it("validates wallet doesn't belong within Merkle Tree", async() => {
-        var proofs = [
-            '0x9682923cee08a0d2c343f252beae1ef85540e977e80fa686488f9f10ea4685ec',
-            '0x48781e8f88ab4d1d07af285d1367732f0dcc4e6f5704640f66ea3c8d3864b713',
-            '0xa05e993355027b3037785d2f63c3363ab61b69e430d4054e364c9566dfb8d0ee',
-            '0xa8e78c35b8fdaa5a0f307cd1d8b12dfa9d0fd99b528ade7bfcbf2b467e35cb6c',
-            '0x7e6e241807eba959c7e886d66fbb24ed7eb9704acfa05c05c31ab7dd5c7d8325',
-            '0x839e6099895ad99f1b5195b401d7b131744def11071dfe8e463f6a0ac1d918a0',
-            '0x7da7c7f5098b4704e5564d19f6d65bc3f551112e46081cd0b70c8cfab4d0a9b0',
-            '0x9a184d9a1744e8e9093ac5e65d7e96c1b609549a75d38c610f538aead2b04c58',
-            '0x7bb0854a5a2a180c8416993d26910d3100cd9f9bc389fb8ac6449d3e37c467d7',
-            '0x7f818777dfe171b443c8a7d57bd2d50e26850f9bd2640b905e5e0b82d205818c'
-          ];
         var leaf = Buffer.from(
             ethers
               .solidityPackedKeccak256(["uint256", "address"], [1002, "0x2AeA83263b22Dc988B4438573040F6536D642a44"])
@@ -113,35 +125,11 @@ describe("Cuy Collection NFT - MINT", function () {
         await expect(result).to.equal(false, "Should recognize the wallet ID doesn't belong to the Merkle tree");
     });
     it("validates whitelist wallet can mint given tokenId", async() => {
-        var proofs = [
-            '0x9682923cee08a0d2c343f252beae1ef85540e977e80fa686488f9f10ea4685ec',
-            '0x48781e8f88ab4d1d07af285d1367732f0dcc4e6f5704640f66ea3c8d3864b713',
-            '0xa05e993355027b3037785d2f63c3363ab61b69e430d4054e364c9566dfb8d0ee',
-            '0xa8e78c35b8fdaa5a0f307cd1d8b12dfa9d0fd99b528ade7bfcbf2b467e35cb6c',
-            '0x7e6e241807eba959c7e886d66fbb24ed7eb9704acfa05c05c31ab7dd5c7d8325',
-            '0x839e6099895ad99f1b5195b401d7b131744def11071dfe8e463f6a0ac1d918a0',
-            '0x7da7c7f5098b4704e5564d19f6d65bc3f551112e46081cd0b70c8cfab4d0a9b0',
-            '0x9a184d9a1744e8e9093ac5e65d7e96c1b609549a75d38c610f538aead2b04c58',
-            '0x7bb0854a5a2a180c8416993d26910d3100cd9f9bc389fb8ac6449d3e37c467d7',
-            '0x7f818777dfe171b443c8a7d57bd2d50e26850f9bd2640b905e5e0b82d205818c'
-          ];
         await contract_CuyCollectionNFT.safeMintWhiteList('0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4',1002, proofs);
         var tokenOwner = await contract_CuyCollectionNFT.ownerOf(1002);
         expect(tokenOwner).to.equal('0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4', "Should have minted the token for the whitelisted wallet");
     });
     it("validates whitelist wallet isn't asking a different tokenId", async() => {
-        var proofs = [
-            '0x9682923cee08a0d2c343f252beae1ef85540e977e80fa686488f9f10ea4685ec',
-            '0x48781e8f88ab4d1d07af285d1367732f0dcc4e6f5704640f66ea3c8d3864b713',
-            '0xa05e993355027b3037785d2f63c3363ab61b69e430d4054e364c9566dfb8d0ee',
-            '0xa8e78c35b8fdaa5a0f307cd1d8b12dfa9d0fd99b528ade7bfcbf2b467e35cb6c',
-            '0x7e6e241807eba959c7e886d66fbb24ed7eb9704acfa05c05c31ab7dd5c7d8325',
-            '0x839e6099895ad99f1b5195b401d7b131744def11071dfe8e463f6a0ac1d918a0',
-            '0x7da7c7f5098b4704e5564d19f6d65bc3f551112e46081cd0b70c8cfab4d0a9b0',
-            '0x9a184d9a1744e8e9093ac5e65d7e96c1b609549a75d38c610f538aead2b04c58',
-            '0x7bb0854a5a2a180c8416993d26910d3100cd9f9bc389fb8ac6449d3e37c467d7',
-            '0x7f818777dfe171b443c8a7d57bd2d50e26850f9bd2640b905e5e0b82d205818c'
-          ];
           await expect(contract_CuyCollectionNFT.safeMintWhiteList('0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4',1003, proofs)).to.revertedWith("Wallet & Token ID combination is not included in the WhiteList");
     });
     
@@ -152,28 +140,72 @@ describe("Cuy Collection NFT - BURN", function () {
         await expect(contract_CuyCollectionNFT.buyBack(44)).to.revertedWith("Token ID is not within the 1000 - 1999 range");
     });
     it("validates token ID cannot be burned by non-owner", async() =>{
-        // var proofs = [
-        //     '0x9682923cee08a0d2c343f252beae1ef85540e977e80fa686488f9f10ea4685ec',
-        //     '0x48781e8f88ab4d1d07af285d1367732f0dcc4e6f5704640f66ea3c8d3864b713',
-        //     '0xa05e993355027b3037785d2f63c3363ab61b69e430d4054e364c9566dfb8d0ee',
-        //     '0xa8e78c35b8fdaa5a0f307cd1d8b12dfa9d0fd99b528ade7bfcbf2b467e35cb6c',
-        //     '0x7e6e241807eba959c7e886d66fbb24ed7eb9704acfa05c05c31ab7dd5c7d8325',
-        //     '0x839e6099895ad99f1b5195b401d7b131744def11071dfe8e463f6a0ac1d918a0',
-        //     '0x7da7c7f5098b4704e5564d19f6d65bc3f551112e46081cd0b70c8cfab4d0a9b0',
-        //     '0x9a184d9a1744e8e9093ac5e65d7e96c1b609549a75d38c610f538aead2b04c58',
-        //     '0x7bb0854a5a2a180c8416993d26910d3100cd9f9bc389fb8ac6449d3e37c467d7',
-        //     '0x7f818777dfe171b443c8a7d57bd2d50e26850f9bd2640b905e5e0b82d205818c'
-        //   ];
         // await contract_CuyCollectionNFT.safeMintWhiteList('0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4',1002, proofs);
         await expect(contract_CuyCollectionNFT.connect(owner).buyBack(1002)).to.revertedWith("You are not the token's owner");
     });
-    // it("validates token ID can be burned by its owner", async() => {
-    //     await expect(contract_CuyCollectionNFT.connect('0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4').buyBack(1002)).to.emit(contract_BBitesToken, "Burn");
-    // });
+    it("validates token ID can be burned by its owner", async() => {
+
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4"],
+          });
+        const signer = await ethers.getSigner("0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4")
+        // console.log(await contract_CuyCollectionNFT.ownerOf(1002));
+        // console.log(whitelistSigner.address);
+        await network.provider.send("hardhat_setBalance", [
+                signer.address,
+                "0x86827644211B00", //valor en hexadecimal
+              ]);
+        expect(await contract_CuyCollectionNFT.connect(signer).buyBack(1002)).to.emit(contract_BBitesToken, "Burn");
+        await hre.network.provider.request({ 
+            method: "hardhat_stopImpersonatingAccount", 
+            params: ["0x007c5e822b66C5463a465ffC17BCf7E02aA9E1A4"] 
+        });
+    });
     it("---testing getting URI for NFT", async() =>{
         // await contract_CuyCollectionNFT.buyBack()
-        await contract_CuyCollectionNFT.connect(owner).safeMint(alice.address, 0);
+        await contract_CuyCollectionNFT.safeMint(alice.address, 0);
         var r = await contract_CuyCollectionNFT.tokenURI(0);
-        console.log("URI: "+r);
+        // console.log("URI: "+r);
     });
 });
+// describe("Public Sale tests", function () {
+//     it("Helper: Price", async() => { 
+
+//         // Token is outside allowed range
+//         await expect(contract_PublicSale.getPriceForId(8880)).to.revertedWith("Token ID must be between 0 and 699");
+
+//         // Common token price
+//         var price = await contract_PublicSale.getPriceForId(100);
+//         expect(price).to.be.equal(1000*10**DECIMALS_BBTKN, `Price for common token should be 100000000 BBTKN and it's at ${price}`);
+
+//         // Rare token price
+//         var price = await contract_PublicSale.getPriceForId(444);
+//         expect(price).to.be.equal(444*20*10**DECIMALS_BBTKN, `Price for rare token is not as expected`);
+
+//         var price_contract = await contract_PublicSale.getPriceForId(666);
+//         days = Math.floor((Date.now() - startDate)/3600*24);
+//         price = 10000+(2000*days);
+//         if (price > 90000) price = 90000;
+//         expect(price_contract).to.be.equal(price*10**DECIMALS_BBTKN, `Price for Legendary token is not as expected`);
+
+//         // TODO: Token is already taken
+//     });
+//     it("BBTKN to buy", async() => {
+        
+//         // buy common
+//         // buy rare
+//         // buy legendary
+//         // buy mystic
+//     });
+//     // buy common
+//     // buy rare
+//     // buy legendary
+//     // buy mystic
+//     // buy via BBTKN
+//     // buy via ether
+//     // buy via USDC
+//     // withdraw ETH
+//     // withdraw tokens BBTKN
+   
+// });
