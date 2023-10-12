173a526b6786e97720867ca381266a967aee4929
Karen Da Cruz <me@karendacruz.com>
2023-10-11 22:30:04 -0500

    Making contract upgradeable, fixing URI metadata

--- contracts/BBitesToken.sol
+++ contracts/BBitesToken.sol
@@ -1,25 +1,44 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-/// @custom:security-contact lee.marreros@blockchainbites.co
-contract BBitesToken is ERC20, Pausable, AccessControl {
+contract BBitesToken is 
+    Initializable, 
+    ERC20Upgradeable, 
+    ERC20PausableUpgradeable, 
+    AccessControlUpgradeable, 
+    ERC20PermitUpgradeable, 
+    UUPSUpgradeable 
+{
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
-    constructor() ERC20("BBites Token", "BBTKN") {
-        _mint(msg.sender, 1000000*10**decimals());
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 
-    // Llamado por el Relayer de Open Zeppelin
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) whenNotPaused {
-        _mint(to, amount*10**decimals());
+    function initialize()
+        initializer public
+    {
+        __ERC20_init("BBites Token", "BBTKN");
+        __ERC20Pausable_init();
+        __AccessControl_init();
+        __ERC20Permit_init("BBites Token");
+        __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -30,11 +49,33 @@ contract BBitesToken is ERC20, Pausable, AccessControl {
         _unpause();
     }
 
+    // Llamado por el Relayer de Open Zeppelin
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) whenNotPaused {
+        _mint(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(UPGRADER_ROLE)
+        override
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    // function _update(address from, address to, uint256 value)
+    //     internal
+    //     override(ERC20Upgradeable, ERC20PausableUpgradeable)
+    // {
+    //     super._update(from, to, value);
+    // }
+
+    ////////
+
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
-    ) internal override whenNotPaused {
+    ) internal override(ERC20Upgradeable, ERC20PausableUpgradeable) whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
--- contracts/CuyCollectionNft.sol
+++ contracts/CuyCollectionNft.sol
@@ -1,42 +1,59 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
-import "hardhat/console.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract CuyCollectionNft is ERC721, Pausable, AccessControl, ERC721Burnable {
+contract CuyCollectionNFT is Initializable, ERC721Upgradeable, ERC721PausableUpgradeable, AccessControlUpgradeable, ERC721BurnableUpgradeable, UUPSUpgradeable {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     bytes32 public root;
 
     event Burn(address account, uint256 id);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor(
-        string memory _name,
-        string memory _symbol
-    ) ERC721(_name, _symbol) {
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize()
+        initializer public
+    {
+        __ERC721_init("Cuy Collection NFT", "CUYNFT");
+        __ERC721Pausable_init();
+        __AccessControl_init();
+        __ERC721Burnable_init();
+        __UUPSUpgradeable_init();
+
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+
         root = 0x2a1e36609eaef943f74318bdadae71c9de4597fd85fdb1e40c4602aa3ef8d5b6;
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://QmTWvm55znTX6NmgopdUpJX8CJsNzhGJY4bJVmMvoJP5hA/";
+        return "ipfs://QmXhXP6zQAzJGaPXGCAf8Xnt17mbYN7HkTAe9f1CDhQARp/";
     }
 
-    function safeMint(
-        address to,
-        uint256 tokenId
-    ) public onlyRole(MINTER_ROLE) whenNotPaused(){
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyRole(MINTER_ROLE) whenNotPaused {
         require(tokenId < 1000, "Only allows token IDs from 0 to 999");
         require(_ownerOf(tokenId)==address(0), "Token ID has already been claimed");
         _safeMint(to, tokenId);
@@ -68,18 +85,40 @@ contract CuyCollectionNft is ERC721, Pausable, AccessControl, ERC721Burnable {
         // Cross chain should emit BBTKN
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(UPGRADER_ROLE)
+        override
+    {}
 
     // The following functions are overrides required by Solidity.
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override(ERC721, AccessControl) returns (bool) {
+
+    // function _update(address to, uint256 tokenId, address auth)
+    //     internal
+    //     //override(ERC721Upgradeable, ERC721PausableUpgradeable)
+    //     override(ERC721Upgradeable)
+    //     returns (address)
+    // {
+    //     return super._update(to, tokenId, auth);
+    // }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
+
+/// 
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal override(ERC721PausableUpgradeable, ERC721Upgradeable) whenNotPaused {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+    }
+
 }
